601316a64b590a428d3e9790ce3c4ed886420082
colt05 <colt05@users.noreply.github.com>
2016-04-16 09:52:42 -0400

    updater updater

--- latestupdater.js
+++ latestupdater.js
@@ -1,1 +1,28 @@
 
+function latestCommit() {
+  var url = "https://api.github.com/repos/colt05/get-request/commits";
+  var response = "[]";
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.open( "GET", url, false);
+  xmlHttp.send( null ); 
+  response = xmlHttp.responseText;
+  var j = JSON.parse(response);
+  return j[0].sha;
+}
+function latestURL() {
+  var hash = latestCommit();
+  var part1 = "//cdn.rawgit.com/colt05/get-request/";
+  var part2 = "/latest.js?hash=";
+  var part3 = Date.now();
+  return part1.concat(hash.concat(part2.concat(part3)));
+}
+function importUpdater() {
+  var callback = function() {importGet()};
+  var head = document.getElementsByTagName('head')[0];
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = latestURL();
+  script.onreadystatechange = callback;
+  script.onload = callback;
+  head.appendChild(script);
+}
