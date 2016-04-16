9f91658d90a66223cbc10c62bf2e9eb8b15ecba9
colt05 <colt05@users.noreply.github.com>
2016-04-16 07:05:53 -0400

    Update get.js

--- get.js
+++ get.js
@@ -1,1 +1,8 @@
-
+function get(url) {
+  var response = "0";
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.open( "GET", url, false );
+  xmlHttp.send( null );
+  response = xmlHttp.responseText;
+  //do not catch error, error handling would not be good
+}
