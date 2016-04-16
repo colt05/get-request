549b8fc9c5dfef507fb7698ddb6d5ec860e9b60e
colt05 <colt05@users.noreply.github.com>
2016-04-16 07:28:49 -0400

    Delete get.js

--- get.js
+++
@@ -1,9 +0,0 @@
-function httpget(url) {
-  var response = "0";
-  var xmlHttp = new XMLHttpRequest();
-  xmlHttp.open( "GET", url, false );
-  xmlHttp.send( null );
-  response = xmlHttp.responseText;
-  //do not catch error, error handling would not be good
-  return response;
-}
