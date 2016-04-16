3f5f541d852d1549695c407157b0dedd05461627
colt05 <colt05@users.noreply.github.com>
2016-04-16 07:54:09 -0400

    Update latest.js

--- latest.js
+++ latest.js
@@ -7,3 +7,9 @@ function latestCommit() {
   var j = JSON.parse(response);
   return j[0].sha;
 }
+function latestURL() {
+  var hash = latestCommit();
+  var part1 = "//cdn.rawgit.com/colt05/get-request/";
+  var part2 = "/get.js";
+  return part1.concat(hash.concat(part2));
+}
