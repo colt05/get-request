5b2f67dc3fd4dbb016a267abd04e9eee0ebebd01
colt05 <colt05@users.noreply.github.com>
2016-04-16 07:26:28 -0400

    Update latest.js

--- latest.js
+++ latest.js
@@ -1,10 +1,10 @@
 function latestCommit() {
-  var url = "api.github.com/repos/colt05/get-request/commits";
+  var url = "//api.github.com/repos/colt05/get-request/commits";
   var response = "0";
   var xmlHttp = new XMLHttpRequest();
   xmlHttp.open( "GET", url, false );
   xmlHttp.send( null );
   response = xmlHttp.responseText;
-  //do not catch error, error handling would not be good
-  return response;
+  var j = JSON.parse(response);
+  return j[0].sha;
 }
